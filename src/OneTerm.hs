f5ecd8c51d71b68e9865440616c9fdc520a64acd
Jiarui Liu <liujiarui382@gmail.com>
2021-07-13 02:34:20 +0800

    oneValidTerm now with size
    Now this function can generate valid terms with given number of nodes. But I think this is not perfect. Because now it can only check the first level of its subterms. How to control the subterm of subterms?
    Here is also a problem. If I only write n<=b (b is the high limit), it can work well. But if I only write n>=a (a is the low limit), when a is larger than 1, the results are all Nothing. Now I don't know why this happens.
    So here I also write n<=b, it can meet the requirement.

--- src/OneTerm.hs
+++ src/OneTerm.hs
@@ -5,23 +5,33 @@ module OneTerm (
 import Test.QuickCheck
 import GetSignatureInfo (allSameTypes)
 import DataType
+import Data.List ((\\))
+import Data.Maybe (catMaybes)
 
-oneValidTerm :: Signature -> Gen Term
-oneValidTerm sig@(Signature fs) = arbTerm sig fs
+oneValidTerm :: Signature -> Int -> Int -> Gen (Maybe Term)
+oneValidTerm sig@(Signature fs) a b = arbTerm sig a b fs
 
---arbTermList :: Signature -> [Type] -> Gen [Term]
---arbTermList sig = mapM (arbTerm sig . allSameTypes sig)
-
---arbTerm :: Signature -> Type -> Gen Term
---arbTerm sig = arbTerm sig . allSameTypes sig
-
-arbTerm :: Signature -> [FunctionSymbol] -> Gen Term
-arbTerm sig fs = do
+arbTerm :: Signature -> Int -> Int -> [FunctionSymbol] -> Gen (Maybe Term)
+arbTerm _ _ _ [] = return Nothing
+arbTerm sig a b fs= do
   one <- elements fs
-  termList <- mapM (arbTerm sig . allSameTypes sig) (arguments one)
-  return (Term (symbol one) termList)
+  termList <- mapM ((arbTerm sig a b . allSameTypes sig)) (arguments one)
+  let termList' = catMaybes termList
+      nList = map number termList'
+      n = sum nList + 1
+  if (n <= b)
+  then return (Just (Term (symbol one) termList'))
+  else arbTerm sig a b (fs\\[one])
 
---oneValidTerm' :: Signature -> Int -> Int -> Gen Term
+number :: Term -> Int
+number fs = 1 + number' (parameters fs)
+
+number' :: [Term] -> Int
+number' [] = 0
+number' (f:fs)
+  | null (parameters f) = 1 + number' fs
+  | otherwise = 1 + number' (parameters f) + number' fs
+
 
 
 
