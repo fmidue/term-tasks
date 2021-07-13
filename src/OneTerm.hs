40997787a5851d6def15f366b6f43ac99bbd161a
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2021-07-13 07:25:30 +0200

    Revert "oneValidTerm now with size"
    This reverts commit f5ecd8c51d71b68e9865440616c9fdc520a64acd.

--- src/OneTerm.hs
+++ src/OneTerm.hs
@@ -5,34 +5,12 @@ module OneTerm (
 import Test.QuickCheck
 import GetSignatureInfo (allSameTypes)
 import DataType
-import Data.List ((\\))
-import Data.Maybe (catMaybes)
 
-oneValidTerm :: Signature -> Int -> Int -> Gen (Maybe Term)
-oneValidTerm sig@(Signature fs) a b = arbTerm sig a b fs
+oneValidTerm :: Signature -> Gen Term
+oneValidTerm sig@(Signature fs) = arbTerm sig fs
 
-arbTerm :: Signature -> Int -> Int -> [FunctionSymbol] -> Gen (Maybe Term)
-arbTerm _ _ _ [] = return Nothing
-arbTerm sig a b fs= do
+arbTerm :: Signature -> [FunctionSymbol] -> Gen Term
+arbTerm sig fs = do
   one <- elements fs
-  termList <- mapM ((arbTerm sig a b . allSameTypes sig)) (arguments one)
-  let termList' = catMaybes termList
-      nList = map number termList'
-      n = sum nList + 1
-  if (n <= b)
-  then return (Just (Term (symbol one) termList'))
-  else arbTerm sig a b (fs\\[one])
-
-number :: Term -> Int
-number fs = 1 + number' (parameters fs)
-
-number' :: [Term] -> Int
-number' [] = 0
-number' (f:fs)
-  | null (parameters f) = 1 + number' fs
-  | otherwise = 1 + number' (parameters f) + number' fs
-
-
-
-
-
+  termList <- mapM (arbTerm sig . allSameTypes sig) (arguments one)
+  return (Term (symbol one) termList)
