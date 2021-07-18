184ab1a981d92164d629050192b160b19488dc94
Jiarui Liu <liujiarui382@gmail.com>
2021-07-19 00:43:56 +0800

    division with new lower bound
    Now the lowest bound of an interval is 1 again. zipWithM is replaced mapM. isAllValiable is removed, this check process is directly done in arbTerm.

--- src/OneTerm.hs
+++ src/OneTerm.hs
@@ -4,8 +4,8 @@ import Test.QuickCheck
 import GetSignatureInfo (allSameTypes)
 import DataType
 import Data.List (transpose)
-import Data.Maybe (catMaybes,isNothing)
-import Control.Monad (replicateM)
+import Data.Maybe (fromJust,isNothing)
+import Control.Monad (replicateM,zipWithM)
 
 oneValidTerm :: Signature -> Int -> Int -> Gen (Maybe Term)
 oneValidTerm sig@(Signature fs) a b = arbTerm sig a b fs
@@ -14,26 +14,30 @@ arbTerm :: Signature -> Int -> Int -> [FunctionSymbol] -> Gen (Maybe Term)
 arbTerm sig a b fs = do
   one <- elements fs
   let n = division (length (arguments one)) a b
-  if a <= 1 && null (arguments one)
+  if a == 1 && null (arguments one)
   then return (Just (Term (symbol one) []))
   else if null n
   then return Nothing
   else do
   n' <- elements n
-  termList <- mapM (\(t,(a',b')) -> arbTerm sig a' b' . allSameTypes sig $ t) (zip (arguments one) n')
-  if isAllValiable termList
-  then return (Just (Term (symbol one) (catMaybes termList)))
-  else return Nothing
+  termList <- zipWithM (\(a',b') t -> arbTerm sig a' b' . allSameTypes sig $ t) n' (arguments one)
+  let termList' = sequence termList
+  if isNothing termList'
+  then return Nothing
+  else return (Just (Term (symbol one) (fromJust termList')))
 
 division ::Int -> Int -> Int -> [[(Int,Int)]]
-division n a b = filter isValidTuples ts
+division n a b
+  | a > b  = []
+  | n >= a = division n (n + 1) b
+  | otherwise = filter isValidTuples ts
                     where ts = map (theTuples . newLists n) (division' n a b)
 
 division' :: Int -> Int -> Int -> [[Int]]
 division' n a b = [x ++ y | x <- validLists a (theLists n a), y <- validLists b (theLists n b)]
 
 theLists ::Int -> Int -> [[Int]]
-theLists n a = replicateM n [0..a-1]
+theLists n a = replicateM n [1..a-1]
 
 validLists :: Int -> [[Int]] -> [[Int]]
 validLists a = filter ((==a-1) . sum)
@@ -61,9 +65,4 @@ number' (f:fs)
   | null (parameters f) = 1 + number' fs
   | otherwise = 1 + number' (parameters f) + number' fs
 
-isAllValiable :: [Maybe Term] -> Bool
-isAllValiable [] = True
-isAllValiable (t:ts)
-  | isNothing t = False
-  | otherwise = isAllValiable ts
 
