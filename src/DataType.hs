c3b9793a54b18e9344b6408d6a4d59cdf848a99a
Jiarui Liu <liujiarui382@gmail.com>
2021-07-21 02:29:17 +0800

    Revert "a new dataType Mode' and new way to generate term"
    This reverts commit 0fad4d1f9aeadf7bd95154867c794e81232aeccb.

--- src/DataType.hs
+++ src/DataType.hs
@@ -5,12 +5,4 @@ data Term = Term {termName :: String, arguments :: [Term]}   deriving (Show,Eq)
 newtype Signature = Signature [FunctionSymbol]   deriving (Show,Eq)
 data FunctionSymbol = FunctionSymbol {symbol :: String, arguments :: [Type], funcType :: Type}   deriving (Show,Eq)
 data Error = ORDER | LENGTH | TYPE | SYMBOL   deriving (Show,Eq)
-data Mode = NONE | NO String | ONCE String   deriving (Show,Eq)
-data Mode' = Mode' Mode (Maybe String)    deriving (Show,Eq)
-
-theSymbol :: Mode -> Maybe String
-theSymbol NONE = Nothing
-theSymbol (NO s) = Just s
-theSymbol (ONCE s) = Just s
-
-
+data Mode = NONE {symbol :: Maybe String} | NO {symbol :: Maybe String} | ONCE {symbol :: Maybe String} deriving (Show,Eq)
