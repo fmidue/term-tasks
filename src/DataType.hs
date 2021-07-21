8929df078c3f12c88d3f0ca6930cbf1a4a346c6f
Oliver Westphal <oliver.westphal@uni-due.de>
2021-07-21 17:45:08 +0200

    add boilerplate for OverloadedLabels

--- src/DataType.hs
+++ src/DataType.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module DataType where
+
+import GHC.OverloadedLabels
+
 newtype Type = Type String   deriving (Show,Eq)
 data Term = Term {termName :: String, arguments :: [Term]}   deriving (Show,Eq)
 newtype Signature = Signature [FunctionSymbol]   deriving (Show,Eq)
@@ -6,4 +12,18 @@ data FunctionSymbol = FunctionSymbol {symbol :: String, arguments :: [Type], fun
 data Error = ORDER | LENGTH | TYPE | SYMBOL   deriving (Show,Eq)
 data Mode = NONE | NO String | ONCE String   deriving (Show,Eq)
 
+-- IsLabel instances --
 
+-- Term
+instance IsLabel "termName" (Term -> String) where
+  fromLabel = termName
+instance IsLabel "arguments" (Term -> [Term]) where
+  fromLabel = arguments
+
+-- FunctionSymbol
+instance IsLabel "symbol" (FunctionSymbol -> String) where
+  fromLabel = symbol
+instance IsLabel "arguments" (FunctionSymbol -> [Type]) where
+  fromLabel = arguments
+instance IsLabel "funcType" (FunctionSymbol -> Type) where
+  fromLabel = funcType
