270b8bf05c4184288f57378535c353eec823d1d8
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2021-08-02 17:39:56 +0200

    consistent use of record label syntax

--- src/GetSignatureInfo.hs
+++ src/GetSignatureInfo.hs
@@ -13,22 +13,22 @@ import DataType (Symbol(..),Signature(..),Type)
 import Data.List (nub,find)
 
 allSymbols :: Signature -> [String]
-allSymbols (Signature fs) = map symbol fs
+allSymbols (Signature fs) = map #symbol fs
 
 allFunctions :: Signature -> [Symbol]
-allFunctions (Signature fs) = filter (not . null . arguments) fs
+allFunctions (Signature fs) = filter (not . null . #arguments) fs
 
 allConstants :: Signature -> [Symbol]
-allConstants (Signature fs) = filter (null . arguments) fs
+allConstants (Signature fs) = filter (null . #arguments) fs
 
 allTypes :: Signature -> [Type]
-allTypes (Signature fs) = nub (concatMap arguments fs ++ map #result fs)
+allTypes (Signature fs) = nub (concatMap #arguments fs ++ map #result fs)
 
 theType :: Signature -> String -> Maybe Type
-theType (Signature fs) s = fmap #result (find ((== s) . symbol) fs)
+theType (Signature fs) s = fmap #result (find ((== s) . #symbol) fs)
 
 theArgumentsTypes :: Signature -> String -> Maybe [Type]
-theArgumentsTypes (Signature fs) s = fmap arguments (find ((== s) . symbol) fs)
+theArgumentsTypes (Signature fs) s = fmap #arguments (find ((== s) . #symbol) fs)
 
 allSameTypes :: Signature -> Type -> [Symbol]
 allSameTypes (Signature fs) t = filter ((== t) . #result) fs
