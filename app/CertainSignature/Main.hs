a1306dd041d5b323f74f33e3fbd18d093c617d3c
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2022-03-07 20:01:19 +0100

    fully integrate the records refactoring

--- app/CertainSignature/Main.hs
+++ app/CertainSignature/Main.hs
@@ -12,9 +12,8 @@ import Records
 
 
 
-withConf :: Certain -> IO ()
+withConf :: Certain -> IO (Bool, [String], [[String]])
 withConf Certain{baseConf = Base{..},..} = do
-    hSetBuffering stdout NoBuffering
     let (a,b) = termSizeRange
         sig' = toSignature signatures
         correctTerms = validTerms sig' Nothing a b
@@ -22,45 +21,49 @@ withConf Certain{baseConf = Base{..},..} = do
     incorrectTerms <- generate(invalidTerms sig' wrongTerms a b `suchThat` (\x->sum (map fst wrongTerms) == sum (map length x)))
     let correctTerms'' = map transTerm correctTerms'
         incorrectTerms' = map (map transTerm) incorrectTerms
-    if properTerms > length correctTerms
-    then putStrLn "Unfortunately, there are not enough correct terms. Here are correct terms given to students:" >> mapM_ print correctTerms''
-    else putStrLn "Here are correct terms given to students:" >> mapM_ print correctTerms''
-    putStrLn "Here are incorrect terms given to students:" >> mapM_ print incorrectTerms'
+    return (properTerms > length correctTerms, correctTerms'', incorrectTerms')
 
 
 
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
+    let
+      Certain
+        { signatures = sig_ex
+        , baseConf = Base
+          { termSizeRange = (a_ex,b_ex)
+          , wrongTerms = e_ex
+          , properTerms = number_ex
+          }
+        }
+        = dCertain
     putStrLn "Whenever you just press Enter, the default value will be taken"
-    let sig_ex = [("x",[],"A"),("y",[],"B"),("z",[],"C"),("f",["A","A"],"B"),("g",["A","B"],"C"),("h",["A","B","C"],"D")]
     putStrLn $ "Please input the signature you want to use (default is [" ++ intercalate "," (transSignature (toSignature sig_ex)) ++ "]): "
     putStrLn "You only need to input name, arguments types and result type. Example: funtion symbol f:A->A->B, written as [\"f\",[\"A\",\"A\"],\"B\"]"
     sig_inp <- getLine
     let sig = (if sig_inp == "" then sig_ex else read sig_inp :: [(String,[String],String)])
-        a_ex = 1
-        b_ex = 10
     putStr $ "Please input the size range [a,b] of terms (default is [" ++ show a_ex ++ "," ++ show b_ex ++ "] ):\na="
     a_inp <- getLine
     let a = (if a_inp == "" then a_ex else read a_inp :: Int)
     putStr "b="
     b_inp <- getLine
     let b = (if b_inp == "" then b_ex else read b_inp :: Int)
-        e_ex = [(5,SWAP)]
     putStrLn ("Please input the error type and number of incorrect terms in this type (default is " ++ show e_ex ++"):\nError types are: " ++ intercalate ", " (map show [minBound .. maxBound :: Error]) ++ ".")
     e_inp <- getLine
     let e = (if e_inp == "" then e_ex else read e_inp :: [(Int,Error)])
-        number_ex = 5
     putStr $ "Please input the number of correct terms you need (default is " ++ show number_ex ++ "):\nnumber="
     number_inp <- getLine
     let number = (if number_inp == "" then number_ex else read number_inp :: Int)
-    let sig' = toSignature sig
-        correctTerms = validTerms sig' Nothing a b
-    correctTerms' <-generate (differentTerms correctTerms (min number (length correctTerms)))
-    incorrectTerms <- generate(invalidTerms sig' e a b `suchThat` (\x->sum (map fst e) == sum (map length x)))
-    let correctTerms'' = map transTerm correctTerms'
-        incorrectTerms' = map (map transTerm) incorrectTerms
-    if number > length correctTerms
+    (tooFewTerms, correctTerms'', incorrectTerms') <-
+      withConf $ Certain
+      { signatures = sig
+      , baseConf = Base
+        {termSizeRange = (a,b)
+        , wrongTerms = e
+        , properTerms = number }
+      }
+    if tooFewTerms
     then putStrLn "Unfortunately, there are not enough correct terms. Here are correct terms given to students:" >> mapM_ print correctTerms''
     else putStrLn "Here are correct terms given to students:" >> mapM_ print correctTerms''
     putStrLn "Here are incorrect terms given to students:" >> mapM_ print incorrectTerms'
--- app/RandomSignature/Main.hs
+++ app/RandomSignature/Main.hs
@@ -4,7 +4,7 @@
 module Main (main) where
 
 import Test.QuickCheck (generate)
-import DataType (Error(..),transTerm,toType,transSignature)
+import DataType (Signature, Error(..),transTerm,toType,transSignature)
 import AllTerm (allTerms)
 import InvalidTerm (differentTerms)
 import System.IO
@@ -12,56 +12,66 @@ import Data.List (intercalate)
 import Records
 
 
-withConf :: Random -> IO ()
+withConf :: Random -> IO (Signature, [String], [[String]])
 withConf Random{baseConf = Base{..},..} = do
-    hSetBuffering stdout NoBuffering
     let (a,b) = termSizeRange
     (sig,(correctTerms,incorrectTerms)) <- generate(allTerms symbols (toType types) wrongTerms maxArgs a b properTerms)
     correctTerms' <- generate (differentTerms correctTerms properTerms)
     let correctTerms'' = map transTerm correctTerms'
         incorrectTerms' = map (map transTerm) incorrectTerms
-    putStrLn "Here are function symbols and constants in the generated signature:" >> mapM_ putStrLn (transSignature sig)
-    putStrLn "Here are correct terms given to students:" >> mapM_ print correctTerms''
-    putStrLn "Here are incorrect terms given to students:" >> mapM_ print incorrectTerms'
+    return (sig, correctTerms'', incorrectTerms')
+
 
 
 
 main ::IO ()
 main = do
     hSetBuffering stdout NoBuffering
+    let
+      Random
+        { symbols = ls_ex
+        , types = types_ex
+        , maxArgs = l_ex
+        , baseConf = Base
+          { termSizeRange = (a_ex,b_ex)
+          , wrongTerms = e_ex
+          , properTerms = number_ex
+          }
+        }
+        = dRandom
     putStrLn "Whenever you just press Enter, the default value will be taken"
-    let ls_ex = ["a","b","c"]
     putStrLn ("Please input the symbols you want to use (default is " ++ show ls_ex ++ "):\nExample: if you want f(x,y), you need to input [\"f\",\"x\",\"y\"]")
     ls_inp <- getLine
     let ls = (if ls_inp == "" then ls_ex else read ls_inp :: [String])
-        types_ex = ["A","B","C"]
     putStrLn ("Please input the type names of types you want to use (default is " ++ show types_ex ++ "):\nExample: the type name of Type \"A\" is \"A\"")
     types_inp <- getLine
     let types = (if types_inp == "" then types_ex else read types_inp :: [String])
-        a_ex = 1
-        b_ex = 10
     putStr $ "Please input the size range [a,b] of terms (default is [" ++ show a_ex ++ "," ++ show b_ex ++ "] ):\na="
     a_inp <- getLine
     let a = (if a_inp == "" then a_ex else read a_inp :: Int)
     putStr "b="
     b_inp <- getLine
     let b = (if b_inp == "" then b_ex else read b_inp :: Int)
-        l_ex = 5
     putStr $ "Please input the maximum number of arguments of a function symbol in this signature (default is " ++ show l_ex ++ "):\nl="
     l_inp <- getLine
     let l = (if l_inp == "" then l_ex else read l_inp :: Int)
-        e_ex = [(5,SWAP)]
     putStrLn ("Please input the error type and number of incorrect terms in this type (default is " ++ show e_ex ++"):\nError types are: " ++ intercalate ", " (map show [minBound .. maxBound :: Error]) ++ ".")
     e_inp <- getLine
     let e = (if e_inp == "" then e_ex else read e_inp :: [(Int,Error)])
-        number_ex = 5
     putStr $ "Please input the number of correct terms you need (default is " ++ show number_ex ++ "):\nnumber="
     number_inp <- getLine
     let number = (if number_inp == "" then number_ex else read number_inp :: Int)
-    (sig,(correctTerms,incorrectTerms)) <- generate(allTerms ls (toType types) e l a b number)
-    correctTerms' <- generate (differentTerms correctTerms number)
-    let correctTerms'' = map transTerm correctTerms'
-        incorrectTerms' = map (map transTerm) incorrectTerms
+    (sig, correctTerms'', incorrectTerms') <-
+      withConf $ Random
+      { symbols = ls
+      , types = types
+      , maxArgs = l
+      , baseConf = Base
+        { termSizeRange = (a,b)
+        , wrongTerms = e
+        , properTerms = number
+        }
+      }
     putStrLn "Here are function symbols and constants in the generated signature:" >> mapM_ putStrLn (transSignature sig)
     putStrLn "Here are correct terms given to students:" >> mapM_ print correctTerms''
     putStrLn "Here are incorrect terms given to students:" >> mapM_ print incorrectTerms'
