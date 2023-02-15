cdc1475491f9d1dfb7289659ec98aece16cc66a0
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2023-02-15 11:10:13 +0100

    code tweaking

--- src/InvalidTerm.hs
+++ src/InvalidTerm.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module InvalidTerm (
   invalidTerms,
 )where
@@ -9,24 +11,23 @@ import ValidTerm (validTerms)
 import Data.List (nub,delete,(\\))
 import Control.Monad (replicateM)
 
-swapArgOrder :: Signature -> Gen (Maybe(Signature,String))
+swapArgOrder :: Signature -> Gen (Maybe (Signature, String))
 swapArgOrder sig@(Signature fs) = do
-    let available = filter (\x -> length (nub (#arguments x)) >=2) fs
+    let available = filter (\x -> length (nub (#arguments x)) >= 2) fs
     if null available
     then return Nothing
     else do
-        one <- elements available
-        (a,b) <- twoDiffPositions (length (#arguments one))
-        if #arguments one !! a == #arguments one !! b
+        Symbol{symbol, arguments, result} <- elements available
+        (a,b) <- twoDiffPositions (length arguments)
+        if arguments !! a == arguments !! b
         then swapArgOrder sig
         else do
-          let newArg = swap a b (#arguments one)
-              newSym = newSymbol(#symbol one)
-              newFs = Symbol newSym newArg (#result one)
-          return (Just(Signature (newFs:fs),newSym))
+          let newArg = swap a b arguments
+              newSym = newSymbol symbol
+              newFs = Symbol newSym newArg result
+          return (Just (Signature (newFs : fs), newSym))
 
 swap :: Int -> Int -> [Type] -> [Type]
-swap _ _ [] = []
 swap n m xs = left ++ [b] ++ middle ++ [a] ++ right
                 where a = xs !! n
                       b = xs !! m
@@ -34,77 +35,67 @@ swap n m xs = left ++ [b] ++ middle ++ [a] ++ right
                       right = drop (m+1) xs
                       middle = take (m-n-1) (drop (n+1) xs)
 
-twoDiffPositions :: Int -> Gen (Int,Int)
-twoDiffPositions 0 = error "This will never happen!"
-twoDiffPositions 1 = error "This will never happen!"
+twoDiffPositions :: Int -> Gen (Int, Int)
+twoDiffPositions n | n < 2 = error "This will never happen!"
 twoDiffPositions n = do
-    a <- chooseInt (0,n-2)
-    b <- chooseInt (0,n-1) `suchThat` (\x -> x/=a && x>a)
-    return (a,b)
+    a <- chooseInt (0,n-1)
+    b <- chooseInt (0,n-1) `suchThat` (/= a)
+    return (min a b, max a b)
 
 newSymbol :: String -> String
 newSymbol s = s ++ "'"
 
-oneMoreArg :: Signature -> Gen (Signature,String)
+oneMoreArg :: Signature -> Gen (Signature, String)
 oneMoreArg sig@(Signature fs) = do
-    one <- elements fs
+    Symbol{symbol, arguments, result} <- elements fs
     oneType <- elements (allTypes sig)
-    position <- chooseInt (0,length (#arguments one)-1)
-    let newArg = newTypes position oneType (#arguments one)
-        newSym = newSymbol(#symbol one)
-        newFs = Symbol newSym newArg (#result one)
-    return (Signature (newFs:fs),newSym)
+    position <- chooseInt (0, length arguments - 1)
+    let newArg = take position arguments ++ [oneType] ++ drop position arguments
+        newSym = newSymbol symbol
+        newFs = Symbol newSym newArg result
+    return (Signature (newFs : fs), newSym)
 
-newTypes :: Int -> Type -> [Type] -> [Type]
-newTypes n t' ts =  take n ts ++ [t'] ++ drop n ts
-
-oneLessArg :: Signature -> Gen (Maybe(Signature,String))
+oneLessArg :: Signature -> Gen (Maybe (Signature, String))
 oneLessArg (Signature fs) = do
-    let available = filter (not. null. #arguments) fs
+    let available = filter (not . null . #arguments) fs
     if null available
     then return Nothing
     else do
-        one <- elements available
-        position <- chooseInt (0,length (#arguments one)-1)
-        let newArg = newTypes' position (#arguments one)
-            newSym = newSymbol(#symbol one)
-            newFs = Symbol newSym newArg (#result one)
-        return (Just(Signature (newFs:fs),newSym))
+        Symbol{symbol, arguments, result} <- elements available
+        position <- chooseInt (0, length arguments - 1)
+        let newArg = [ t | (i, t) <- zip [0..] arguments, i /= position ]
+            newSym = newSymbol symbol
+            newFs = Symbol newSym newArg result
+        return (Just (Signature (newFs : fs), newSym))
 
-newTypes' :: Int -> [Type] -> [Type]
-newTypes' n ts = [t | (i,t) <- zip [0..] ts, i /= n]
-
-oneDiffType :: Signature -> Gen (Maybe(Signature,String))
+oneDiffType :: Signature -> Gen (Maybe (Signature, String))
 oneDiffType sig@(Signature fs) = do
-    let available = filter (not. null. #arguments) fs
+    let available = filter (not . null . #arguments) fs
     if null available
     then return Nothing
     else do
-        one <- elements available
-        position <- chooseInt (0,length (#arguments one)-1)
+        Symbol{symbol, arguments, result} <- elements available
+        position <- chooseInt (0, length arguments - 1)
         let types = allTypes sig
-            newList = delete (#arguments one !! position) types
+            newList = delete (arguments !! position) types
         if null newList
         then return Nothing
         else do
-            t <- elements newList
-            let newArg = replace position t (#arguments one)
-                newSym = newSymbol(#symbol one)
-                newFs = Symbol newSym newArg (#result one)
-            return (Just(Signature (newFs:fs),newSym))
+            t' <- elements newList
+            let newArg = [ if i == position then t' else t | (i, t) <- zip [0..] arguments ]
+                newSym = newSymbol symbol
+                newFs = Symbol newSym newArg result
+            return (Just (Signature (newFs : fs), newSym))
 
-replace :: Int -> Type -> [Type] -> [Type]
-replace n t' ts = [if i == n then t' else t | (i,t) <- zip [0..] ts ]
-
-wrongSymbol :: Signature -> Gen (Signature,String)
+wrongSymbol :: Signature -> Gen (Signature, String)
 wrongSymbol (Signature fs) = do
-    one <- elements fs
-    let newSym = newSymbol(#symbol one)
+    Symbol{symbol, arguments, result} <- elements fs
+    let newSym = newSymbol symbol
         newSym' = newSymbol newSym
-        newFs = Symbol newSym' (#arguments one) (#result one)
-    return (Signature (newFs:fs),newSym')
+        newFs = Symbol newSym' arguments result
+    return (Signature (newFs : fs), newSym')
 
-wrongSymbol' :: Signature -> Gen (Maybe(Signature,String))
+wrongSymbol' :: Signature -> Gen (Maybe (Signature, String))
 wrongSymbol' sig@(Signature fs) = do
     let types = allTypes sig
         argsAndRes = allArgsResults sig
@@ -114,12 +105,12 @@ wrongSymbol' sig@(Signature fs) = do
     then return Nothing
     else do
         let symbols = allSymbols sig
-        (typeList,t) <- elements available
+        (typeList, t) <- elements available
         newSym <- elements symbols
         let newSym' = newSymbol newSym
             newSym'' = newSymbol newSym'
             newFs = Symbol newSym'' typeList t
-        return (Just(Signature (newFs:fs),newSym''))
+        return (Just (Signature (newFs : fs), newSym''))
 
 combination :: Int -> [Type] -> [([Type],Type)]
 combination n ts = [(x,t) | i <- reverse [0..n], t <- ts, x <- replicateM i ts ]
