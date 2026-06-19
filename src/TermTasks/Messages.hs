f81d5e36dd6eb0225a6c255cb7d325c398b8237a
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2026-06-19 14:33:58 +0200

    Update Messages.hs

--- src/TermTasks/Messages.hs
+++ src/TermTasks/Messages.hs
@@ -20,8 +20,8 @@ text1 = paragraph $ translate $ do
 
 text2 :: OutputCapable m => LangM m
 text2 = paragraph $ translate $ do
-    german "Entscheiden Sie für die folgenden Terme, ob es sich um korrekte Terme im Sinne der oben gegebenen Deklarationen handelt:"
-    english "Decide for the following terms whether or not they are correct terms according to the declarations given above:"
+    german "Entscheiden Sie für die folgenden Optionen, ob es sich um korrekte Terme gemäß der oben gegebenen Deklarationen handelt:"
+    english "Decide for the following choices whether or not they are correct terms according to the declarations given above:"
 
 text3 :: OutputCapable m => LangM m
 text3 = do
@@ -29,6 +29,6 @@ text3 = do
     german "Geben Sie Ihre Antwort in Form einer Liste von Zahlen an, die genau alle korrekten Terme enthält."
     english "State your answer by giving a list of numbers, indicating exactly all correct terms."
   paragraph $ translate $ do
-    german "Zum Beispiel würde [1, 2] bedeuten, dass nur die Terme 1. und 2. der gegebenen korrekt sind."
-    english "For example, [1, 2] would mean that only terms 1. and 2. of the given ones are correct."
+    german "Zum Beispiel würde [1, 2] bedeuten, dass nur die Terme 1. und 2. der gegebenen Optionen korrekt sind."
+    english "For example, [1, 2] would mean that only terms 1. and 2. of the given choices are correct."
   pure ()
